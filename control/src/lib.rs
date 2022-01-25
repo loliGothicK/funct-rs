19705ec908da2c0a054bca8ec90dcea6b694ed2e
Mitama <yussa.de.dannann@gmail.com>
2022-01-25 11:36:19 +0900

    feat: lift_a2

--- control/src/lib.rs
+++ control/src/lib.rs
@@ -1,20 +1,29 @@
 #![feature(generic_associated_types)]
 
+use std::convert::identity;
+
 trait Functor {
     type Underlying;
-    type Rebind<B>: Functor;
+    type Rebind<T>;
 
     fn fmap<F, B>(self, f: F) -> Self::Rebind<B>
     where
         F: FnOnce(Self::Underlying) -> B;
+
+    fn replace<B>(self, fb: Self::Rebind<B>) -> Self::Rebind<B>;
 }
 
 trait Applicative: Functor {
     fn pure(a: Self::Underlying) -> Self;
     fn apply<A, B, FA>(self, fa: FA) -> Self::Rebind<B>
-        where
-            FA: Functor<Underlying = A, Rebind<B> = Self::Rebind<B>>,
-            Self::Underlying: FnOnce(<FA as Functor>::Underlying) -> B;
+    where
+        FA: Functor<Underlying = A, Rebind<B> = Self::Rebind<B>>,
+        Self::Underlying: FnOnce(<FA as Functor>::Underlying) -> B;
+    fn lift_a2<F, A, B, C, FA, FB>(f: F, fa: FA, fb: FB) -> Self::Rebind<C>
+    where
+        FA: Functor<Underlying = A, Rebind<A> = Option<A>>,
+        FB: Functor<Underlying = B, Rebind<C> = Option<C>>,
+        F: FnOnce(<FA as Functor>::Underlying, <FB as Functor>::Underlying) -> C;
 }
 
 impl<T> Functor for Option<T> {
@@ -27,6 +36,10 @@ impl<T> Functor for Option<T> {
     {
         self.map(f)
     }
+
+    fn replace<B>(self, fb: Self::Rebind<B>) -> Self::Rebind<B> {
+        fb
+    }
 }
 
 impl<T> Applicative for Option<T> {
@@ -35,14 +48,22 @@ impl<T> Applicative for Option<T> {
     }
 
     fn apply<A, B, FA>(self, fa: FA) -> Self::Rebind<B>
-        where
-            FA: Functor<Underlying = A, Rebind<B> = Self::Rebind<B>>,
-            Self::Underlying: FnOnce(<FA as Functor>::Underlying) -> B
+    where
+        FA: Functor<Underlying = A, Rebind<B> = Self::Rebind<B>>,
+        Self::Underlying: FnOnce(<FA as Functor>::Underlying) -> B,
     {
         self.map_or(None, |f| fa.fmap(f))
     }
-}
 
+    fn lift_a2<F, A, B, C, FA, FB>(f: F, fa: FA, fb: FB) -> Self::Rebind<C>
+    where
+        FA: Functor<Underlying = A, Rebind<A> = Option<A>>,
+        FB: Functor<Underlying = B, Rebind<C> = Option<C>>,
+        F: FnOnce(<FA as Functor>::Underlying, <FB as Functor>::Underlying) -> C,
+    {
+        fa.fmap(identity).and_then(|a| fb.fmap(|b| f(a, b)))
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -58,5 +79,10 @@ mod tests {
     fn apply() {
         let add = Some(|x| x + 1);
         assert_eq!(add.apply(Some(1)), Some(2));
+    }
+
+    #[test]
+    fn lift_a2() {
+        assert_eq!(Option::<i32>::lift_a2(|x, y| x + y, Some(1), Some(1)), Some(2));
     }
 }
